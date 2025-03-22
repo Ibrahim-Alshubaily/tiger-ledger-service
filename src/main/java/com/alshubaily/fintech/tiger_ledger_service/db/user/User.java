56a0e560c590c0110a072bec8f049d7974786517
Ibrahim Alshubaily <ibrahim.alshubaily.dev@gmail.com>
2025-03-22 13:34:10 +0300

    Add users psql table

---
+++ src/main/java/com/alshubaily/fintech/tiger_ledger_service/db/user/User.java
@@ -0,0 +1,28 @@
+package com.alshubaily.fintech.tiger_ledger_service.db.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
